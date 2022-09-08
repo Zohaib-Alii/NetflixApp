b8f17fe14eea3530a6b5aeb8185f56c470a92cda
Noman ahmed <noman.ahmed@datapillar.ai>
2022-09-08 16:46:22 +0100

    UI section close to complete (9-8-22)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.scss";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Home />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import Feature from "../../components/feature/Feature";
+import List from "../../components/list/List";
+import Navbar from "../../components/navbar/Navbar";
+import Login from "../login/Login";
+import Register from "../register/Register";
+import "./home.scss";
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      l<Feature type="movie" />
+      <List />
+      <List />
+      <Register />
+      <Login />
+    </div>
+  );
+};
+
+export default Home;
